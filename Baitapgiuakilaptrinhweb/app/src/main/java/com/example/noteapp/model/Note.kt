1b17518b2aff6c0553c5668a73ecbf6ed351e6dd
quangtrong72 <tranlequangtrong@gmail.com>
2026-05-12 01:41:53 +0700

    update

--- Baitapgiuakilaptrinhweb/app/src/main/java/com/example/noteapp/model/Note.kt
+++
@@ -1,9 +0,0 @@
-package com.example.noteapp.model
-
-data class Note(
-    var id: String = "",
-    var title: String = "",
-    var description: String = "",
-    var fileUrl: String = "",
-    var time: Long = 0L
-)
